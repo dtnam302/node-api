a29e71b5aa65adbccf8c0840fc4e639809d6b9f6
Thanh Nam <dtnam302@gmail.com>
2022-05-07 09:41:23 +0700

    fake commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -68,7 +68,11 @@ const postController = {
         post.rooms.push(newRoom._id);
       }
       await post.save();
-      return res.status(200).json({ result: Response(post) });
+      Post.findOne({ _id: postID })
+        .populate({ path: "rooms", select: "name" })
+        .exec((err, post) => {
+          return res.status(200).json({ result: Response(post) });
+        });
     }
   },
   createHotspot: async (req, res, next) => {
